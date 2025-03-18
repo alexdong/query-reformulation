86280c6c00be041924cf6d0537e85cedd795e8e7
Alex Dong <190415+alexdong@users.noreply.github.com>
2025-03-19 09:44:10 +1300

    refactor: Improve show_capabilities function with type hints

--- train/check_gpu.py
+++ train/check_gpu.py
@@ -1,7 +1,6 @@
 import torch
 
-# Interrogates the PyTorch library to determine the available hardware and software capabilities on this machine. It'll also print out and return the version, options value etc for each backend. this function will be used at starting up to determine the size of the model and training hyperparameters used. Please also add a __main__ for me to exercise it. ai!
-def show_capabilities():
+def show_capabilities(): # add type hints, ai!
     """
     Interrogates the PyTorch library to determine the available hardware and software capabilities.
     Returns a dictionary with information about available hardware and PyTorch configuration.
